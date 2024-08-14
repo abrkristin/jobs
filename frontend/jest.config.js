fe624659f47e395368827614b6f0a854c7efd965
abrkristin <126964452+abrkristin@users.noreply.github.com>
2024-08-14 17:00:34 +0400

    Update jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '^.+\\.js$': 'babel-jest',
   },
+  moduleFileExtensions: ['js', 'vue', 'json'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  testEnvironment: 'jsdom',
 };
